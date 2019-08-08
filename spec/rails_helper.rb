a86eea42f5087a659266108f6a032bcc8d91fae9
Daria Lykova <daria.lykova@gmail.com>
2019-08-08 18:50:01 +0200

    Creates a support folder and a response_json file for parsing the body of the server back json/Requires the file in rails_helper and includes it in rspec configuration/Refactors pings spec/Rspec green

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'support/response_json.rb'
 
 ActiveRecord::Migration.maintain_test_schema!
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
@@ -17,6 +18,7 @@ RSpec.configure do |config|
     config.filter_rails_from_backtrace!
     config.include FactoryBot::Syntax::Methods
     config.include Shoulda::Matchers::ActiveRecord, type: :model
+    config.include ResponseJSON, type: :request 
 end
 
 Shoulda::Matchers.configure do |config|
--- spec/requests/api/v0/pings_spec.rb
+++ spec/requests/api/v0/pings_spec.rb
@@ -5,9 +5,10 @@ RSpec.describe Api::V0::PingsController, type: :request do
     it 'should return Pong' do
       get '/api/v0/pings'
 
-      json_response = JSON.parse(response.body)
+      response_json = JSON.parse(response.body)
       expect(response.status).to eq 200
-      expect(json_response['message']).to eq 'Pong'
+      expect(response_json['message']).to eq 'Pong'
+
     end
   end
 
