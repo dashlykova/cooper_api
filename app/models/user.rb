8c9819aa19b84c18a8c58b164e5878c97b5638da
Daria Lykova <daria.lykova@gmail.com>
2019-08-08 20:33:50 +0200

    Adding settings in devise token auth initializer to make it work with the client/Generates Performance Data & adds hstore datatype in order to enable hash storing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  has_many :performance_data, class_name: 'PerformanceData'
   extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+  
 end
