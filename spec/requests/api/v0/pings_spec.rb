8ef3ea395634163e73a6ec6ae2a168a050ab78a0
Daria Lykova <daria.lykova@gmail.com>
2019-08-08 13:52:48 +0200

    Writes first request spec

--- spec/requests/api/v0/pings_spec.rb
+++ spec/requests/api/v0/pings_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Api::V0::PingsController, type: :request do
       json_response = JSON.parse(response.body)
       expect(response.status).to eq 200
       expect(json_response['message']).to eq 'Pong'
-
     end
   end
 
