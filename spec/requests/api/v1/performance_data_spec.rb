180e705e5f22d9db73316ea535b2a013ca73bc41
Daria Lykova <daria.lykova@gmail.com>
2019-08-09 10:44:15 +0200

    Writes the remaining request spec tests for performance data

--- spec/requests/api/v1/performance_data_spec.rb
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
   let(:user) { FactoryBot.create(:user) }
   let(:credentials) { user.create_new_auth_token }
   let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
+  let(:not_headers) { {HTTP_ACCEPT: 'application/json'} }
 
   describe 'POST /api/v1/performance_data' do
     it 'creates a data entry' do
@@ -12,5 +13,27 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
       entry = PerformanceData.last
       expect(entry.data).to eq 'message' => 'Average'
     end
+  end
+
+  describe 'GET /api/v1/performance_data' do
+    before do
+      5.times { user.performance_data.create(data: { message: 'Average' }) }
+    end
+
+    it 'returns the collection of performance data' do
+      get '/api/v1/performance_data', headers: headers
+      expect(response_json['entries'].count).to eq 5
+    end
+
+    it 'does not return a collection of performance data if user is not logged in' do
+      get '/api/v1/performance_data', headers: not_headers
+      expect(response_json['errors']).to eq ["You need to sign in or sign up before continuing."]
+    end
+
+    it 'returns a collection of performance data that belongs to the logged in user' do
+      get '/api/v1/performance_data', headers: headers
+      expect((response_json['entries']).first['user_id']).to eq user['id']
+    end
+
   end
 end
